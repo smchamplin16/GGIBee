0d42afcd978f47bbaca6ef327b698688a2e1dda8
samira <samirachamplin@gmail.com>
2017-07-13 10:50:07 -0500

    Mazeflower script (not working), edge collider on big maze

--- Assets/Scripts/mazeFlower.cs
+++ Assets/Scripts/mazeFlower.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class mazeFlower : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Sprite bud;
+    Collider2D flowerCollider;
+    public bool used;
+
+    // Use this for initialization
+    void Start () {
+        flowerCollider = GetComponent<Collider2D>();
+        used = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Bee" && used) {
+            flowerCollider.enabled = false;
+            GetComponent<SpriteRenderer>().sprite = bud;
+        }
+    }
 }
