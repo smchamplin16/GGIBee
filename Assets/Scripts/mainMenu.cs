c7c0dd62726009780b3f60b950273d35e0c6614b
samira <samirachamplin@gmail.com>
2017-07-13 11:52:00 -0500

    Main menu button working

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class mainMenu : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<Button>().onClick.AddListener(() => TaskOnClick());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TaskOnClick() { // go to level select
+        Debug.Log("button clicked");
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+}
