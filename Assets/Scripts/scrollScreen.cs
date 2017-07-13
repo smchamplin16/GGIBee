4a4417531edace169b536b47195e76ded4ae36f6
samira <samirachamplin@gmail.com>
2017-07-13 14:36:29 -0500

    Edits to screen scrolling (not working)

--- Assets/Scripts/scrollScreen.cs
+++ Assets/Scripts/scrollScreen.cs
@@ -7,6 +7,11 @@ public class scrollScreen : MonoBehaviour {
     public float dragSpeed = 2;
     private Vector3 dragOrigin;
 
+    public bool camDrag = true;
+
+    public float outerLeft = -10f;
+    public float outerRight = 10f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,18 +19,33 @@ public class scrollScreen : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown(0)) {
-            dragOrigin = Input.mousePosition;
-            return;
+
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        float left = Screen.width * 0.2f;
+        float right = Screen.width - left;
+
+        if(mousePosition.x < left) {
+            camDrag = true;
+        } else if (mousePosition.x > right) {
+            camDrag = true;
         }
 
-        if (!Input.GetMouseButton(0)) {
-            return;
+        if (camDrag) {
+            if (Input.GetMouseButtonDown(0)) {
+                dragOrigin = Input.mousePosition;
+                return;
+            }
+
+            if (!Input.GetMouseButton(0)) {
+                return;
+            }
+
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
+
+            transform.Translate(move, Space.World);
+
         }
-
-        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-        Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
-
-        transform.Translate(move, Space.World);
 	}
 }
