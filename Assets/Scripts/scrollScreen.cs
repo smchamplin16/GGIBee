fa0899b486011dfb6a3fd96badf941ca42831055
samira <samirachamplin@gmail.com>
2017-07-14 12:21:55 -0500

    Scrolling progress

--- Assets/Scripts/scrollScreen.cs
+++ Assets/Scripts/scrollScreen.cs
@@ -35,10 +35,10 @@ public class scrollScreen : MonoBehaviour {
 
         Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
-        float left = 0.2f;
-        float right = Screen.width - 0.2f;
-        float top = Screen.height - 0.2f;
-        float bottom = 0.2f;
+        float left = 0.5f;
+        float right = Screen.width - 0.5f;
+        float top = Screen.height - 0.5f;
+        float bottom = 0.5f;
 
         if (maxX > right && minX < left && maxY > top && minY < bottom) {
             camDrag = true;
@@ -72,14 +72,32 @@ public class scrollScreen : MonoBehaviour {
             }
 
             Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-            Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
+            Debug.Log("dragOrigin: " + dragOrigin);
+            Debug.Log("mousePos: " + mousePosition);
+            Debug.Log("pos: " + pos);
 
-            if (!camUp || !camDown) {
-                move = new Vector3(pos.x * dragSpeed, 0, 0);
+            Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
+            Debug.Log("move: " + move);
+
+            if (!camUp) {
+                if(move.y < 0f) {
+                    move = new Vector3(move.x, 0, 0);
+                }
+                
+            } else if (!camDown) {
+                if(move.y > 0f) {
+                    move = new Vector3(move.x, 0, 0);
+                }
             }
 
-            if (!camLeft || !camRight) {
-                move = new Vector3(0, pos.y * dragSpeed, 0);
+            if (!camLeft) {
+                if(move.x > 0f) {
+                    move = new Vector3(0, move.y, 0);
+                }
+            } else if (!camRight) {
+                if(move.x < 0f) {
+                    move = new Vector3(0, move.y, 0);
+                }
             }
             
             transform.Translate(move, Space.World);
