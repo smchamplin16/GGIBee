b451cb8f35ddfc09ebe2ece434e8f4d6f30d99d8
samira <samirachamplin@gmail.com>
2017-07-19 20:01:51 -0500

    Scene selection works

---
+++ Assets/Scripts/selectLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class selectLevel : MonoBehaviour {
+
+    public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Button>().onClick.AddListener(() => TaskOnClick());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TaskOnClick() { // go to level select
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
