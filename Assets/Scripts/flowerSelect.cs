c23ff5262e341f8c28f1fe778999ae272ec9cfc7
samira <samirachamplin@gmail.com>
2017-07-31 11:15:21 -0500

    Flower selector chooses random flower to display

---
+++ Assets/Scripts/flowerSelect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flowerSelect : MonoBehaviour {
+    
+    private List<GameObject> children;
+
+	// Use this for initialization
+	void Start () {
+        children = new List<GameObject>();
+
+		foreach(Transform child in transform) {
+            children.Add(child.gameObject);
+        }
+        
+        children[Random.Range(0, children.Count)].SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
