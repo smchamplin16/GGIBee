fdfe9e57f9e93d201a1ffc8bff1fad3020ab2e1c
samira <samirachamplin@gmail.com>
2017-08-01 13:15:43 -0500

    Flowers open and trigger disables on collide

---
+++ Assets/Scripts/flowerGet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flowerGet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D() {
+        GetComponent<CircleCollider2D>().enabled = false;
+        GetComponent<Animator>().enabled = true;
+    }
+}
