ce6af242e7652597d0c9133ec560be4d041d9deb
samira <samirachamplin@gmail.com>
2017-08-11 11:09:32 -0500

    Made flower selector more space efficient
    Now instantiates clone from list of prefabs

--- Assets/Scripts/flowerSelect.cs
+++ Assets/Scripts/flowerSelect.cs
@@ -7,17 +7,19 @@ public class flowerSelect : MonoBehaviour {
     public List<GameObject> children;
     public GameObject currentChild;
     public bool randomize;
+    private bool isInstantiated;
 
     void Awake() {
-        children = new List<GameObject>();
+        /*children = new List<GameObject>();
 
         foreach (Transform child in transform) {
             children.Add(child.gameObject);
-        }
+        }*/
     }
 
 	// Use this for initialization
 	void Start () {
+        isInstantiated = false;
         if (randomize) {
             float x = Random.value;
             if(x < .01f) {
@@ -31,16 +33,19 @@ public class flowerSelect : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (!currentChild.activeSelf) {
-            currentChild.SetActive(true);
+        if (!isInstantiated) {
+            Instantiate(currentChild, this.transform);
+            isInstantiated = true;
         }
 	}
 
     public void flowerReset() {
-        currentChild.GetComponent<SpriteRenderer>().sprite = currentChild.GetComponent<flowerGet>().startSprite;
-        currentChild.GetComponent<Animator>().enabled = false;
-        currentChild.GetComponent<CircleCollider2D>().enabled = true;
-        currentChild.SetActive(false);
+        //currentChild.GetComponent<SpriteRenderer>().sprite = currentChild.GetComponent<flowerGet>().startSprite;
+        //currentChild.GetComponent<Animator>().enabled = false;
+        //currentChild.GetComponent<CircleCollider2D>().enabled = true;
+        //currentChild.SetActive(false);
+        Destroy(transform.GetChild(0).gameObject);
+        isInstantiated = false;
         float x = Random.value;
         if (x < .01f) {
             currentChild = children[children.Count-1];
