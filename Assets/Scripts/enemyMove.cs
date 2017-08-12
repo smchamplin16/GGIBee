407c49a2f7a01e341488e8f63974d8cc39a19b1e
samira <samirachamplin@gmail.com>
2017-08-12 00:14:30 -0500

    Fixed enemy back and forth mechanic

---
+++ Assets/Scripts/enemyMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMove : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Background") {
+            transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z + 180);
+        }
+    }
+}
