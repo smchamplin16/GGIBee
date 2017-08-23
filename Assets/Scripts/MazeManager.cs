3a2f535cd9394596f87f1f981a9d0b9f6d7848da
samira <samirachamplin@gmail.com>
2017-08-23 12:04:23 -0500

    Fixed win/loss overlap, repositioned pollen

--- Assets/Scripts/MazeManager.cs
+++ Assets/Scripts/MazeManager.cs
@@ -7,23 +7,29 @@ public class MazeManager : MonoBehaviour {
 
     public GameObject[] flowers;
     public bool win;
+    private bool hasWon;
+    private bool hasLost;
     public GameObject winScreen;
     public bool lose;
     public GameObject loseScreen;
     public AudioClip winSound;
     public AudioClip loseSound;
     private AudioSource source;
+    private GameObject bee;
 
 	// Use this for initialization
 	void Start () {
         source = GetComponent<AudioSource>();
         win = false;
         lose = false;
+        hasWon = false;
+        bee = GameObject.FindGameObjectWithTag("Bee");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (win) {
+        if (win && !hasLost) {
+            hasWon = true;
             if (!winScreen.activeSelf) {
                 source.PlayOneShot(winSound);
                 winScreen.SetActive(true);
@@ -31,7 +37,8 @@ public class MazeManager : MonoBehaviour {
             }
         }
 
-        if(lose) {
+        if(lose && !hasWon) {
+            hasLost = true;
             if (!loseScreen.activeSelf) {
                 loseScreen.SetActive(true);
                 StartCoroutine("WaitAndReset");
@@ -40,11 +47,16 @@ public class MazeManager : MonoBehaviour {
 	}
 
     IEnumerator WaitAndReset() {
+        bee.GetComponent<BeeMove>().enabled = false;
         yield return new WaitForSeconds(3);
         if (lose) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         } else if (win) {
-            SceneManager.LoadScene("level_selectv2");
+            if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings - 1) {
+                SceneManager.LoadScene("level_selectv2");
+            } else {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
         }
     }
 }
