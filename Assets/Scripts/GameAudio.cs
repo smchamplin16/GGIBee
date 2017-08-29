21a4bf2737dc6fbc8e554ffd5d86eeb59dc857a4
samira <samirachamplin@gmail.com>
2017-08-29 12:00:39 -0500

    Made persisting game audio

---
+++ Assets/Scripts/GameAudio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAudio : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        DontDestroyOnLoad(transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
