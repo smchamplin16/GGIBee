0d1ca9da6da218f612749bc9ed60eb50fa7b1a03
samira <samirachamplin@gmail.com>
2017-08-29 22:52:11 -0500

    Made game audio a singleton

--- Assets/Scripts/GameAudio.cs
+++ Assets/Scripts/GameAudio.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class GameAudio : MonoBehaviour {
 
+    private static GameAudio instance = null;
+    public static GameAudio Instance {
+        get { return instance; }
+    }
+    public bool audioOn;
+
 	// Use this for initialization
 	void Awake () {
-        DontDestroyOnLoad(transform.gameObject);
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
 	}
 	
 	// Update is called once per frame
